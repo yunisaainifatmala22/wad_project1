fcb458710829960b1ecb53f5df915250ac9b3e37
yunisaainifatmala22 <146923149+yunisaainifatmala22@users.noreply.github.com>
2023-10-19 17:49:48 +0700

    Add files via upload

---
+++ wad_project2/latihan1.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Latihan WAD</title>
+  </head>
+  <body>
+    <h1>Hello Yunisa Aini Fatmala Good Morning!!</h1>
+  </body>
+</html>
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "Hello Yunisa Aini Fatmala Good Morning!!";
+?>
+
+</body>
+</html>
